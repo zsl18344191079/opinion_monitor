b1ad32740c3df2e7844b91f5c0d0daf1785d9a8f
itzzh <1434317359@qq.com>
2019-12-25 16:07:16 +0800

    增加django-allauth的基础设置

--- opinion_monitor/settings.py
+++ opinion_monitor/settings.py
@@ -35,10 +35,65 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # django-allauth必须安装的app
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    # 第三方账号相关，根据需求添加
+    'allauth.socialaccount.providers.weibo',
+    'allauth.socialaccount.providers.github',
+    # Django模板语言样式库
+    'widget_tweaks',
+    # restful
     'rest_framework',
+    # 模型
+    'Myaccount.apps.MyaccountConfig',
     'MBlog.apps.MblogConfig',
-    'monitor',
+    'monitor.apps.MonitorConfig',
 ]
+
+# django-allauth相关设置
+AUTHENTICATION_BACKENDS = (
+    # django admin所使用的用户登录与django-allauth无关
+    'django.contrib.auth.backends.ModelBackend',
+    # allauth 身份验证
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+# app django.contrib.sites需要的设置
+SITE_ID = 1
+# 指定要使用的登录方法(用户名、电子邮件地址两者之一)
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+# smtp 服务器地址
+EMAIL_HOST = "smtp.qq.com"
+# 要求用户注册时必须填写email
+ACCOUNT_EMAIL_REQUIRED = True
+# 注册成功后，会发送一封验证邮件，用户必须验证邮箱后，才能登陆
+ACCOUNT_EMAIL_VERIFICATION = "optional"
+# 邮箱确认邮件的截止日期(天数)
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 3
+# 更改为True，用户一旦确认他们的电子邮件地址，就会自动登录
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
+# 登录或注册后自动跳转到/accounts/profile/
+LOGIN_REDIRECT_URL = '/accounts/profile/'
+# 默认端口25，若请求超时可尝试465
+EMAIL_PORT = 465
+# 用户名
+EMAIL_HOST_USER = "1434317359@qq.com"
+# 邮箱代理授权码（不是邮箱密码）
+EMAIL_HOST_PASSWORD = "afdddmhlalgdjfhh"
+# 是否使用了SSL 或者TLS（两者选其一）
+# EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
+# 发送人
+EMAIL_FROM = "1434317359@qq.com"  #
+# 默认显示的发送人，（邮箱地址必须与发送人一致），不设置的话django默认使用的webmaster@localhost
+DEFAULT_FROM_EMAIL = "微博與情 <1434317359@qq.com>"
+
+# 如果使用自定义用户model user，为了让 Django 能够识别自定义的用户模型，必须在此处添加app.自定义表名
+AUTH_USER_MODEL = 'Myaccount.User'
+# ___________________________________________________________________________________________________________________________________
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -123,3 +178,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'wb_opinion/dist')]
+
+
+# 媒体文件专属参数设置（需要加url识别才能访问）
+MEDIA_URL = "/media/"  # 媒体文件别名(相对路径) 和 绝对路径
+
+# 存放上传的文件
+MEDIA_ROOT = (
+    os.path.join(BASE_DIR, 'media')
+)
--- opinion_monitor/urls.py
+++ opinion_monitor/urls.py
@@ -15,14 +15,21 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from django.conf.urls.static import static
 
 # from django.views.generic.base import TemplateView
+from opinion_monitor import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('monitor.urls')),
-    url(r'^', include('MBlog.urls')),
-    url(r'^api-auth/', include('rest_framework.urls')),
-    # path('', TemplateView.as_view(template_name="index.html"))
-]
+                  path('admin/', admin.site.urls),
+                  path('api/', include('monitor.urls')),
+                  url(r'^', include('MBlog.urls')),
+                  url(r'^api-auth/', include('rest_framework.urls')),
+                  # django-allauth插件
+                  re_path(r'^accounts/', include('allauth.urls')),
+                  # django-allauth用户信息扩展
+                  re_path(r'^accounts/', include('Myaccount.urls', namespace='accounts')),
+                  # path('', TemplateView.as_view(template_name="index.html"))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # 加入这个才能显示media文件
